02db1f1c9a4d1a409645fad5d4d4eb7d05ea0096
Mark Buckle <markbuckle92@gmail.com>
2024-10-15 15:48:17 -0230

    Implement passport local strategy

--- server/app.js
+++ server/app.js
@@ -1,56 +1,71 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-// package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require('connect-mongo');
-const cookieParser = require('cookie-parser');
+var passport = require('passport');
+var crypto = require('crypto');
+var routes = require('./routes');
+const connection = require('./config/database');
+const MongoStore = require('connect-mongo'); // Package documentation - https://www.npmjs.com/package/connect-mongo
 
-// create the express application
+/**
+ * -------------- GENERAL SETUP ----------------
+ */
+
+// Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
+require('dotenv').config();
+
+// Create the Express application
 var app = express();
-// Connect to the DB
-// in this case we are using the local mongo server. This will change if in production
-const dbString = 'mongodb://localhost:27017/HealthLyncDatabase';
-// const dbString = 'mongodb://localhost:27017/?readPreference=primary&appname=MongoDBtest';
-// const dbOptions = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }
-// const connection = mongoose.createConnection(dbString, dbOptions);
 
-// middleware 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser()); 
 
-// a sessions data is stored on the server side
+/**
+ * -------------- SESSION SETUP ----------------
+ */
+
 app.use(session({
-    // usually stored in an environment variable but we'll leave it this way for setup purposes
-    secret: 'some secret',
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl: dbString,
-        // mongoOptions: dbOptions,
-        collectionName: 'users'
-        }),
-        // a cookie's data is stored in the browser
-        cookie: {
+        mongoUrl: process.env.DB_STRING, // Ensure this is a valid string
+        collectionName: 'users' // Use collectionName instead of collection
+      }),
+    cookie: {
         // expiration property
             maxAge: 24 * 60 * 60 * 1000 // Equals 1 day (24 hr/ day * 60min/1 hr * 60sec/min * 1000ms/sec)
         }
 }));
 
-app.get('/', (req, res, next) => {
+// TODO
 
-    if (req.session.viewCount) {
-        // if we have that property on the session, increment it by one
-        req.session.viewCount = req.session.viewCount + 1;
-        res.send(`<h1>You have visited this page ${req.session.viewCount} times.</h1>`)
-    } else {
-        req.session.viewCount = 1;
-        res.send(`<h1>You have visited this page ${req.session.viewCount} time.</h1>`)
-    }
-    // res.send(`<h1>You have visited this page ${req.session.viewCount} time</h1>`)
+/**
+ * -------------- PASSPORT AUTHENTICATION ----------------
+ */
+require('./config/passport'); // Need to require the entire Passport config module so app.js knows about it
+
+app.use(passport.initialize()); // initialize passport middleware
+app.use(passport.session()); // serialize/deserialize user
+
+// runs for any route in the application
+app.use((req, res, next) => {
+    console.log(req.session);
+    console.log(req.user);
+    next();
 });
 
+/**
+ * -------------- ROUTES ----------------
+ */
+
+// Imports all of the routes from ./routes/index.js
+app.use(routes);
+
+
+/**
+ * -------------- SERVER ----------------
+ */
+
+// Server listens on http://localhost:3000
 app.listen(3000);
