2ba34b533310eba712cf7865c90bfb35b0f5ef12
Mark Buckle <markbuckle92@gmail.com>
2024-10-18 07:51:13 -0230

    Move files not being used

--- server/middleware/archive/authStrategies.js
+++ server/middleware/archive/authStrategies.js
@@ -4,7 +4,7 @@ const LocalStrategy = require('passport-local').Strategy;
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const { User } = require('../models/user');
+const { User } = require('../../models/user');
 const bcrypt = require('bcrypt');
 
 // Local Strategy
